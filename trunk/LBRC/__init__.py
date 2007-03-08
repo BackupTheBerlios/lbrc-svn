d225d6e4886f603fa473e1c45488fd1ec399bd2e
linuxfun <linuxfun@f0bdfa2c-52f3-4846-b196-e1a9aad26a61>
2007-03-08 13:16:32 +0000

    add conveniance function to acquire dbus interface

--- trunk/LBRC/__init__.py
+++ trunk/LBRC/__init__.py
@@ -1,7 +1,28 @@
+import dbus
 import sys
 import os.path as osp
 
 scriptpath = osp.dirname(osp.abspath(sys.argv[0]))
+
+def dinterface(bus, service, obj_path, interface):
+    """
+    Convenience function to make the interface creation from a
+    dbus object, the service, object path and interface much more
+    smooth
+
+    @param  bus:        the DBUS bus, where the service resides
+    @type   bus:        dbus.Bus
+    @param  service:    the service, we call the object from
+    @type   service:    string
+    @param  obj_path:   path to the object
+    @type   obj_path:   string
+    @param  interface:  the interface, we want to acquire for the interface
+    @type   interface:  string
+    @return:    Requested DBUS Interface
+    @rtype:     dbus.Interface
+    """
+    proxy_obj = bus.get_object(service, obj_path)
+    return dbus.Interface(proxy_obj, interface)
 
 def get_localedir():
     """
