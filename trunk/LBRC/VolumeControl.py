7bb93762c9b09a3b527454217feb593cb6866571
linuxfun <linuxfun@f0bdfa2c-52f3-4846-b196-e1a9aad26a61>
2007-11-25 11:36:57 +0000

    switch to gstreamer
    with that we can now support at least oss and alsa implementing
    support for esd/nas/pulseaudio should be easy, if the correct
    plugins are installed

--- trunk/LBRC/VolumeControl.py
+++ trunk/LBRC/VolumeControl.py
@@ -1,30 +1,79 @@
+# TODO: Check how much overhead is produced by invoking the gstreamer constructor each time
 from LBRC.Listener import Listener
 import logging
 import gobject
+import gst
+import gst.interfaces
 import re
 from subprocess import Popen, PIPE, call
 
 class VolumeCommand(object):
+    percent_re = re.compile('([+-])(\d+)%')
+    
     def __init__(self, parent, description):
         self.parent = parent
         self.description = description
+    
+    def _volume_change(self):
+        logging.debug("VolumeControl: Start of volume_change")
+        option_set = self.parent.default_properties.copy()
+        option_set.update(self.description)
+        if not "change" in option_set or \
+           not "sound_system" in option_set:
+            logging.error("Incomplete option_set for Volume Change: " + str(option_set))
+            return
+        
+        if option_set['sound_system'] == 'alsa':
+            mixer = gst.element_factory_make('alsamixer')
+        elif option_set['sound_system'] == 'oss':
+            mixer = gst.element_factory_make('ossmixer')
+        else:
+            logging.error('Unbekanntes Sound System: ' + option_set['sound_system'])
+            return
+        
+        logging.debug("VolumeControl: Pre set_property")
+        if 'device' in option_set:
+            mixer.set_property('device', option_set['device'])
+        mixer.set_state(gst.STATE_PAUSED)
+        if not 'channel' in option_set:
+            track = mixer.list_tracks()[0]
+        else:
+            track = None
+            for t in mixer.list_tracks():
+                if t.label == option_set['channel']:
+                    track = t
+                    break
+            if track == None:
+                logging.error('Specified channel not found!')
+        
+        logging.debug("VolumeControl: Pre change")
+        match = self.percent_re.match(option_set['change'])
+        if (match):
+            logging.debug('change volume')
+            old_volume = mixer.get_volume(track)
+            logging.debug('old volume: ' + str(old_volume))
+            change = 1
+            if match.group(1) == '-':
+                change = -1
+            change *= int(match.group(2)) * (track.max_volume-track.min_volume) / 100
+            new_volume = [ov + change for ov in old_volume]
+            mixer.set_volume(track, tuple(new_volume))
+        elif option_set['change'] == 'toggle':
+            logging.debug('toggle mute')
+            mixer.set_mute(track, (not gst.interfaces.MIXER_TRACK_MUTE & track.flags))
+        mixer.set_state(gst.STATE_NULL)
+        logging.debug("VolumeControl: End of volume_change")
+            
     
     def call(self):
         description = self.description
         logging.debug("VolumeControl - Doing call for: " + str(description))
         if 'change' in description:
-            command = [
-                       "/usr/bin/amixer",
-                       "-c",
-                       str(description["card"]),
-                       'sset',
-                       str(description["channel"]),
-                       str(description["change"])
-                       ]
-            call( command )
-            logging.debug("VolumeControl - Call done: " + str(command))
+            try:
+                self._volume_change()
+            except Exception, e:
+                logging.debug(str(e))
             self.parent.sendVolumeUpdate()
-            
         elif 'ShowVolumeControl' in description:
             query = {'type': "displayControl", 
                      'command': 'showModule',
@@ -40,7 +89,12 @@ class VolumeCommand(object):
             
             logging.debug("Calling VolumeUpdate")
             self.parent.sendVolumeUpdate()
-            
+        elif 'SetDefault' in description:
+            logging.debug("Setting defaults")
+            try:
+                self.parent.default_properties.update(description['SetDefault'])
+            except Exception, e:
+                logging.debug(str(e))
         elif 'ShowChannels' in description:
             self.parent.setVisibleDevices(description['ShowChannels']);
 
@@ -55,24 +109,46 @@ class VolumeControl(Listener):
         """
         Listener.__init__(self, config, 'VolumeControl', command_class=VolumeCommand)
         self.devices = []
+        self.default_properties = {}
 
     def setVisibleDevices(self, devices):
         self.devices = []
         for device in devices:
-            self.devices.append({"card": device[0], "channel": device[1]})
+            option_set = self.default_properties.copy()
+            option_set.update(device)
+            self.devices.append(option_set)
+        logging.debug("end setVisibleDevices")
 
     @staticmethod
-    def _getVolume(card, channel):
-        output = Popen(["/usr/bin/amixer", "-c", str(card), 'sget', str(channel)], stdout=PIPE).communicate()[0]
-        regexp = re.compile("\[(\d+)%\]")
-        for line in output.split("\n"):
-            if "Playback" in line and "%]" in line:
-                logging.debug('match')
-                ret = regexp.search(line)
-                if not ret:
-                    next
-                else:
-                    return ret.group(1)
+    def _getVolume(option_set):
+        if not "channel" in option_set or \
+           not "sound_system" in option_set:
+            logging.error("Incomplete option_set for Volume Change: " + str(option_set))
+            return
+        
+        if option_set['sound_system'] == 'alsa':
+            mixer = gst.element_factory_make('alsamixer')
+        elif option_set['sound_system'] == 'oss':
+            mixer = gst.element_factory_make('ossmixer')
+        else:
+            logging.error('Unbekanntes Sound System: ' + option_set['sound_system'])
+            return
+        
+        logging.debug("VolumeControl: Pre set_property")
+        if 'device' in option_set:
+            mixer.set_property('device', option_set['device'])
+        mixer.set_state(gst.STATE_PAUSED)
+        track = None
+        for t in mixer.list_tracks():
+            if t.label == option_set['channel']:
+                track = t
+                break
+        if track == None:
+            logging.error('Specified channel not found!')
+        logging.debug("end _getVolume")
+        volume = int(100 * mixer.get_volume(track)[0] / track.max_volume)
+        mixer.set_state(gst.STATE_NULL)
+        return str(volume)
 
     def sendVolumeUpdate(self):
         query = {'type': "VolumeControl",
@@ -80,8 +156,8 @@ class VolumeControl(Listener):
                  "volumeData": []
                  }
         for dev in self.devices:
-            name = str(dev['card']) + " - " + str(dev['channel'])
-            volume = self._getVolume(dev['card'], dev['channel'])
+            name = str(dev['channel'])
+            volume = self._getVolume(dev)
             query['volumeData'].append([name, volume])
         bc = self.bluetooth_connector.get_bt_connection()
         if bc != None:
