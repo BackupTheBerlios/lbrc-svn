89d456f3f0a2d6762e8714d22bee5c4641bec841
Matthias Bläsing <Matthias Blsing@f0bdfa2c-52f3-4846-b196-e1a9aad26a61>
2008-07-28 19:42:17 +0000

    beautify CommandExecutor

--- trunk/LBRC/CommandExecutor.py
+++ trunk/LBRC/CommandExecutor.py
@@ -3,12 +3,19 @@ import logging
 import gobject
 
 class Command(object):
+    """
+    Command class for the CommandExecutorModule
+    """
     def __init__(self, parent, description):
         self.parent = parent
         self.logger = logging.getLogger("LBRC.Listener.CommandExecutor")
         self.description = description
 
     def _to_array(self):
+        """
+        The commandline arguments are linearized (other implementations could
+        need them seperately, so we keep the format)
+        """
         command_line = []
         command_line.append(self.description['command']) 
         if 'arguments' in self.description:
@@ -16,15 +23,20 @@ class Command(object):
         return command_line
     
     def call(self):
+        """
+        Execute the supplied command by spawning a subprozess
+        """
         self.logger.debug("Calling: " + str(self._to_array()))
         gobject.spawn_async( self._to_array(), 
                              flags= gobject.SPAWN_STDOUT_TO_DEV_NULL | 
                                     gobject.SPAWN_STDERR_TO_DEV_NULL )
 
 class CommandExecutor(Listener):
+    """
+    CommandExecutor class provides a mechanism to call external programms
+    """
     def __init__(self, config):
-        Listener.__init__(self, config, "CommandExecutor", command_class=Command);
-                                
-    def shutdown(self):
-        for command in self.destruct:
-            command.call()
+        Listener.__init__(self,
+                          config, 
+                          "CommandExecutor", 
+                          command_class=Command)
